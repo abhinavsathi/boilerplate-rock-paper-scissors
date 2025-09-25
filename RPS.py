21511417c90840a14cbfd72be55f105916686505
Abhinav Sathiajith <sathiajithabhinav@gmail.com>
2025-09-25 14:05:53 +0000

    Completed Rock Paper Scissors project

--- RPS.py
+++ RPS.py
@@ -1,10 +1,40 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 
-def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+def player(prev_play, opponent_history=[], my_history=[]):
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    if prev_play != "":
+        opponent_history.append(prev_play)
+        if len(my_history) > 0:
+            my_history.append(my_history[-1])
 
-    return guess
+    if len(opponent_history) < 20:
+        return "R"
+
+    #Kris Strategy
+    if len(my_history) > 0 and prev_play == counter(my_history[-1]):
+        return counter(counter(my_history[-1]))
+
+    #Rest of Bots Strategy
+    patterns = {}
+    n = 4  
+    if len(opponent_history) > n:
+        for i in range(len(opponent_history) - n):
+            key = tuple(opponent_history[i:i+n])
+            if key not in patterns:
+                patterns[key] = []
+            patterns[key].append(opponent_history[i+n])
+
+        last_moves = tuple(opponent_history[-n:])
+        if last_moves in patterns:
+            predicted_move = max(set(patterns[last_moves]), key=patterns[last_moves].count)
+            return counter(predicted_move)
+
+    return "P"
+
+def counter(move):
+    if move == "R":
+        return "P"
+    if move == "P":
+        return "S"
+    if move == "S":
+        return "R"
